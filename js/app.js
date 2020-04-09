5b617df161893d18014fd42853c82a2af6f656a4
KIMBERLY DAMALAS <kimatcamp@yahoo.com>
2020-04-09 06:06:35 -0700

    cookies per hour calc correct

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 //Global Variables
-hours: ['6am','7am','8am','9am','10am','11am','12pm',
+var hoursArray = ['6am','7am','8am','9am','10am','11am','12pm',
   '1pm','2pm','3pm','4pm','5pm','6pm','7pm'];
-
+var simCPHArray = [];
+var total = 0;
 
 //Object literal for each location
 //CPH = Customer Per Hour
@@ -10,36 +11,46 @@ hours: ['6am','7am','8am','9am','10am','11am','12pm',
 //dynamically add/store each loop: rdmCPH = random CPH;
 //hourCPC = hourly (sim based on random); locationTotal = sum (sim cookies over hours)
 
-var Seattle = {
+var seattle = {
   name: 'Seattle',
   minCPH: 23,
   maxCPH: 65,
   avgCPC: 6.3,
-  simCPH: [],
 
   //generate random number of CPH
   //Objects/Math/random (inclusive)
   randomCPH: function(){
     var rdmNumber = Math.floor(Math.random() * (this.maxCPH - this.minCPH + 1)) + this.minCPH;
+    console.log('random' + rdmNumber);
     return rdmNumber;
   },
-
-  simulateCPH: function(){
-    //calculate sim cookies per each hour
-    //using avgCPC and randomCPH
-    //store value for each hour 6am-7pm
-    //add with location.hourCPC: XX
-  },
-
-  locationTotal: function(){
-    //calculate
-    //update with sum (location.simCPC, previous)
-    //add with location.locationTotal
-    //display sales.html
-  },
-
 };
 
-// wrapped in for loop location.hours.length for display
+//calculate sim cookies per each hour using avgCPC and randomCPH
+//store in simCPHArray = simulated cookies per hour
+function simulateCPH(){
+  return simCPHArray[i]= seattle.randomCPH() * seattle.avgCPC;
+}
 
-console.log(Seattle.randomCPH());
+for (var i = 0; i < hoursArray.length; i ++){
+  simulateCPH();
+  simCPHArray.push();
+  console.log (' sim[i]' + simCPHArray[i]);
+}
+
+//calculate total cookies for the day
+function totalCookies(index){
+  total = total + simCPHArray[index];
+  return total;
+}
+
+for (i = 0; i <= simCPHArray.length; i ++){
+  totalCookies(i);
+  console.log ('total' + totalCookies());
+}
+
+
+
+// Display via DOM manipulation
+
+console.log(seattle);
