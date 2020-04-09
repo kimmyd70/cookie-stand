f9cb1cb50a3246835ccd155b376973e5b42d6324
KIMBERLY DAMALAS <kimatcamp@yahoo.com>
2020-04-08 21:13:26 -0700

    random number gen done

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
+//Global Variables
+hours: ['6am','7am','8am','9am','10am','11am','12pm',
+  '1pm','2pm','3pm','4pm','5pm','6pm','7pm'];
+
+
 //Object literal for each location
 //CPH = Customer Per Hour
 //CPC  = Cookie Per Customer
@@ -5,42 +10,36 @@
 //dynamically add/store each loop: rdmCPH = random CPH;
 //hourCPC = hourly (sim based on random); locationTotal = sum (sim cookies over hours)
 
-var seattle = {
-    minCPH: 23,
-    maxCPH: 65,
-    avgCPC: 6.3,
-  
-// wrapped in for loop w/if for each hour (0-14): 6-11am, 12pm, 1-5pm, 6-7pm
-  
+var Seattle = {
+  name: 'Seattle',
+  minCPH: 23,
+  maxCPH: 65,
+  avgCPC: 6.3,
+  simCPH: [],
+
   //generate random number of CPH
   //Objects/Math/random (inclusive)
-  randomCPH: function(){  
-    min = Math.ceil(this.minCPH),
-    max = Math.floor(this.maxCPH),
-    seattle.rdmCPH = Math.floor(Math.random() * (max - min + 1)) + min,
-  }
+  randomCPH: function(){
+    var rdmNumber = Math.floor(Math.random() * (this.maxCPH - this.minCPH + 1)) + this.minCPH;
+    return rdmNumber;
+  },
 
-  setMaxMinCPH: function(){
-    //IF to store CPH as location.minCPH or location.maxCPH as appropriate
-    console.log (seattle.rdmCPH)
-  }
-
-  simCPC: function(){
+  simulateCPH: function(){
     //calculate sim cookies per each hour
     //using avgCPC and randomCPH
-    
-
     //store value for each hour 6am-7pm
     //add with location.hourCPC: XX
+  },
 
-  }
-  
   locationTotal: function(){
-      //calculate
-      //update with sum (location.simCPC, previous)
-      //add with location.locationTotal
-      //display sales.html
-    }
+    //calculate
+    //update with sum (location.simCPC, previous)
+    //add with location.locationTotal
+    //display sales.html
+  },
 
-  // for loop w/hours complete
 };
+
+// wrapped in for loop location.hours.length for display
+
+console.log(Seattle.randomCPH());
